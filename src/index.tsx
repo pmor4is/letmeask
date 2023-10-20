306076967bc590ce67e8284fd96318311e074bf6
Pedro Morais <pmor4is@gmail.com>
2023-10-20 19:30:24 -0300

    Adicionando fontes, estilos globais iniciais

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Importação de serviços do Firebase
 import './services/firebase';
+// Importação do estilo global da aplicação. Ele altera qualquer componente dentro da aplicação
+// .scss: extensão da biblioteca node-sass. Sass é um pré-processador css
+import './styles/global.scss';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
