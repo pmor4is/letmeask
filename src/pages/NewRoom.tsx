e570594cdfca2261546a7a02f33122799274eac5
Pedro Morais <pmor4is@gmail.com>
2023-10-20 20:44:02 -0300

    Página de nova sala e sua estilização, no mesmo arquivo auth.scss

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,38 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+
+import '../styles/auth.scss'
+import {Button} from '../components/Button'
+
+// Todo componente deve estar com a letra inicial em maísculo. Toda tag HTML é em minúsculo. Ao colocar maiúsculo na primeira letra,o React entende que é um componente.
+export function NewRoom() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt='Ilustração simbolizando perguntas e respostas' />
+                {/* Em HTML, o '&' é utilizado para criar simbolos. Então para utilizar o &, deve escrever &amp; */}
+                {/* strong: conteúdo de grande relevância */}
+                <strong>Crie salas de Q&amp;A ao vivo</strong>
+                <p>Tire as dúvidas da sua audência em tempo real</p>
+            </aside>
+
+            <main>
+                <div className='main-content'>
+                    <img src={logoImg} alt='Let me ask' />
+
+                    <h2>Criar uma nova sala</h2>
+                     
+                    <form>
+                        <input type="text" placeholder="Nome da sala" />
+                        <Button type="submit">Criar sala</Button>
+                    </form>
+
+                    <p>
+                        Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+                    </p>
+                </div>
+            </main>
+        </div>
+    )
+}
